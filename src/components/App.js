85c04f0822d4c0a6e10acc1f18f7bb345811acf6
jeremy perez <glyde06@hotmail.com>
2017-08-02 21:43:57 -0500

    Created 'actions', 'components' and 'reducers' folders in src folder
    I'm going to end up using Redux, so might as well go ahead and structure the app accordingly

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     return (
       <div>
         <div>
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React yall</h2>
         </div>
         <p>
           To get started, edit <code>src/App.js</code> and save to reload.
