f6f1f4c337828a4675920fbfe8b80d551d803419
jeremy perez <glyde06@hotmail.com>
2017-08-02 22:29:16 -0500

    Add templates for some components I plan on creating

---
+++ src/components/event_detail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function EventDetail() {
+  return (
+    <div>
+      <h2>Event</h2>
+    </div>
+  )
+}
+
+export default EventDetail;
---
+++ src/components/search_bar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function SearchBar() {
+  return (
+    <div>
+      <h2>Search...</h2>
+    </div>
+  )
+}
+
+export default SearchBar;
