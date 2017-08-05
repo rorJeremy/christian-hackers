b4f203b46291e834d8079244b142a3f7b11bed7d
jeremy perez <glyde06@hotmail.com>
2017-08-04 21:42:15 -0500

    Import and render the search_bar component in the App component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 
 function SearchBar() {
   return (
-    <div>
-      <h2>Search...</h2>
-    </div>
+    <input />
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 // import registerServiceWorker from './registerServiceWorker';
 
 function App() {
   return (
     <div>
       <h2>Christian Hackers</h2>
+      <SearchBar />
     </div>
   )
 }
