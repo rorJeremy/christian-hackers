5f3ddcc429b2868ac71eea382b5d1f311bd88d01
jeremy perez <glyde06@hotmail.com>
2017-08-04 21:33:29 -0500

    Remove the App.js component file
    Instead of putting the application layout component in it's own file, I'm just going to include it in our
    main index.js file.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 // import registerServiceWorker from './registerServiceWorker';
+
+function App() {
+  return (
+    <div>
+      <h2>Christian Hackers</h2>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
