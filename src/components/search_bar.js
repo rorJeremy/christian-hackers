15d5def6aabd7b845f6cbdf1b0fe70ba0cfe4ae8
jeremy perez <glyde06@hotmail.com>
2017-08-04 22:19:59 -0500

    Bind the event handler method in the constructor
    We bind the event handler in the constructor, because a bind call in the render path like this:
    <input onChange={this.onInputChange.bind(this)} />
    creates a brand new function on every single render.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
 
   onInputChange() {
   	
