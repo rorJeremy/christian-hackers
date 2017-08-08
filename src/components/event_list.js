26e818d0065f1246d5c3a973a445729be3d3e761
jeremy perez <glyde06@hotmail.com>
2017-08-07 20:40:57 -0500

    Loop over events array prop/state in EventList component

--- src/components/event_list.js
+++ src/components/event_list.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import EventDetail from './event_detail';
 
-function EventList() {
+function EventList(props) {
   return (
     <ul>
+      {props.events.map((event, index) => (
+        <EventDetail
+          key={event.key}
+        />
+      ))}    
     </ul>
   )
 }
