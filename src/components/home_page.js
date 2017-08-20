729ed3d1ab2b2e308788c49a04083f060ba65a2c
jeremy perez <glyde06@hotmail.com>
2017-08-19 19:56:30 -0500

    Remove App component and add HomePage component

---
+++ src/components/home_page.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function HomePage(props) {
+  return (
+    <p>
+    	Home Page
+    </p>
+  )
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -9,44 +9,14 @@ import SearchBar from './components/search_bar';
 import EventDetail from './components/event_detail';
 import EventList from './components/event_list';
 import Header from './components/header';
+import HomePage from './components/home_page';
 // import registerServiceWorker from './registerServiceWorker';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      events: [{key: "1", title: "event1", description: "the first event"}, {key: "2", title: "event2", description: "the second event"}],
-      selectedEvent: null,
-      searchTerm: ''
-    };
-    this.onSearchChange = this.onSearchChange.bind(this);
-  }
-
-  onSearchChange(term) {
-    this.setState({ searchTerm: term.target.value });
-  }  
-
-  render() {
-	  return (
-	    <div>
-	      <SearchBar value={this.state.searchTerm} onSearchTermChange={this.onSearchChange} />
-	      <EventDetail event={this.state.selectedEvent} />
-	      <EventList 
-	      	onEventSelect={selectedEvent => this.setState({ selectedEvent })}
-	      	events={this.state.events}
-	      	term={this.state.searchTerm}
-	      />
-	    </div>
-	  )
-	}
-}
 
 ReactDOM.render(
   <Router>
   	<div>
   		<Header />
-      <Route exact path="/" component={App}/>
+      <Route exact path="/" component={HomePage}/>
       <Route path="/events" component={EventList}/>
     </div>
   </Router>
