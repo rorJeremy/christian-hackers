e58f56f6620a73e2e31370b81b606fe86e7cf62e
jeremy perez <glyde06@hotmail.com>
2017-08-19 19:51:50 -0500

    Add searchbar to EventList component

--- src/components/event_list.js
+++ src/components/event_list.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SearchBar from './search_bar';
 import EventListItem from './event_list_item';
 
 const isSearched = (searchTerm) => (item) => !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -8,20 +9,29 @@ class EventList extends React.Component {
     super(props);
 
     this.state = {
-      events: [{key: "1", title: "event1", description: "the first event"}, {key: "2", title: "event2", description: "the second event"}]
+      events: [{key: "1", title: "event1", description: "the first event"}, {key: "2", title: "event2", description: "the second event"}],
+      searchTerm: ''
     };
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onSearchChange(term) {
+    this.setState({ searchTerm: term.target.value });
   }
 
   render() {
     return (
-      <ul>
-        {this.state.events.map((event, index) => (
-          <EventListItem
-            key={event.key}
-            event={event}
-          />
-        ))}    
-      </ul>
+      <div>
+        <SearchBar value={this.state.searchTerm} onSearchTermChange={this.onSearchChange} />
+        <ul>
+          {this.state.events.filter(isSearched(this.state.searchTerm)).map((event, index) => (
+            <EventListItem
+              key={event.key}
+              event={event}
+            />
+          ))}    
+        </ul>
+      </div>
     )    
   }
 }
