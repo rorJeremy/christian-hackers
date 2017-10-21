c84b95ca1dad99bd394532332c0e18bf2be138fe
jeremy perez <glyde06@hotmail.com>
2017-10-21 01:28:54 -0500

    Add a bootstrap navbar to the header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Grid, Navbar, Nav, NavItem } from 'react-bootstrap';
 
 function Header() {
   return (
-    <h2>
-      <Link to="/">Christian Hackers</Link>
-    </h2>
+    <Navbar inverse fixedTop>
+      <Grid>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Christian Hackers</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem>
+              <Link to="/">About</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/events">Events</Link>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Grid>
+    </Navbar>
   );
 }
 
