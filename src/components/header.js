e93771f66fe8465751e739502926ee9f33014c65
jeremy perez <glyde06@hotmail.com>
2017-10-21 00:53:49 -0500

    Import the bootstrap css files into the Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 function Header() {
   return (
