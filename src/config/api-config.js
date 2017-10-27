f91802f5618ec37708ffd57f323e9d80117180b4
jeremy perez <glyde06@hotmail.com>
2017-10-27 00:15:21 -0500

    Use a regex for assigning api_root when on generic zeit

--- src/config/api-config.js
+++ src/config/api-config.js
@@ -5,8 +5,8 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === 'christianhackers.com') {
   backendHost = 'https://christian-hackers-api.herokuapp.com';
-} else if (hostname === 'christianhackers-staging.herokuapp.com') {
-  backendHost = 'https://christian-hackers-api-staging.herokuapp.com';
+} else if (new RegExp('christian-hackers').test(hostname)) {
+  backendHost = 'https://christian-hackers-api.herokuapp.com';
 } else {
   backendHost = 'http://localhost:3002';
 }
